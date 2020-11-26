542dcff125b3480ef779d71465345f183c080c13
Karol Sójko <karolsojko@gmail.com>
2020-11-26 14:10:30 +0100

    fix: typeorm inner joins

--- src/Infra/MySQL/MySQLRevisionRepository.spec.ts
+++ src/Infra/MySQL/MySQLRevisionRepository.spec.ts
@@ -21,36 +21,34 @@ describe('MySQLRevisionRepository', () => {
   })
 
   it('should find revisions by item id', async () => {
-    queryBuilder.innerJoin = jest.fn().mockReturnThis()
-    queryBuilder.where = jest.fn().mockReturnThis()
+    queryBuilder.innerJoinAndSelect = jest.fn().mockReturnThis()
     queryBuilder.getMany = jest.fn().mockReturnValue([revision])
 
     const result = await repository.findByItemId('123')
 
-    expect(queryBuilder.innerJoin).toHaveBeenCalledWith(
-      'item_revisions',
-      'ir',
-      'ir.item_uuid = revision:item_uuid'
+    expect(queryBuilder.innerJoinAndSelect).toHaveBeenCalledWith(
+      'revision.items',
+      'item',
+      'item.uuid = :item_uuid',
+      { item_uuid: '123' }
     )
-    expect(queryBuilder.where).toHaveBeenCalledWith('ir.item_uuid = :item_uuid', { item_uuid: '123' })
     expect(result).toEqual([revision])
   })
 
   it('should find one revision by id and item id', async () => {
     queryBuilder.where = jest.fn().mockReturnThis()
-    queryBuilder.andWhere = jest.fn().mockReturnThis()
-    queryBuilder.innerJoin = jest.fn().mockReturnThis()
+    queryBuilder.innerJoinAndSelect = jest.fn().mockReturnThis()
     queryBuilder.getOne = jest.fn().mockReturnValue(revision)
 
     const result = await repository.findOneById('123', '234')
 
     expect(queryBuilder.where).toHaveBeenCalledWith('revision.uuid = :uuid', { uuid: '234' })
-    expect(queryBuilder.innerJoin).toHaveBeenCalledWith(
-      'item_revisions',
-      'ir',
-      'ir.item_uuid = revision:item_uuid'
+    expect(queryBuilder.innerJoinAndSelect).toHaveBeenCalledWith(
+      'revision.items',
+      'item',
+      'item.uuid = :item_uuid',
+      { item_uuid: '123' }
     )
-    expect(queryBuilder.andWhere).toHaveBeenCalledWith('ir.item_uuid = :item_uuid', { item_uuid: '123' })
     expect(result).toEqual(revision)
   })
 })
--- src/Infra/MySQL/MySQLRevisionRepository.ts
+++ src/Infra/MySQL/MySQLRevisionRepository.ts
@@ -8,24 +8,24 @@ import { RevisionRepositoryInterface } from '../../Domain/Revision/RevisionRepos
 export class MySQLRevisionRepository extends Repository<Revision> implements RevisionRepositoryInterface {
   async findByItemId(itemId: string): Promise<Array<Revision>> {
     return this.createQueryBuilder('revision')
-      .innerJoin(
-        'item_revisions',
-        'ir',
-        'ir.item_uuid = revision:item_uuid'
+      .innerJoinAndSelect(
+        'revision.items',
+        'item',
+        'item.uuid = :item_uuid',
+        { item_uuid: itemId }
       )
-      .where('ir.item_uuid = :item_uuid', { item_uuid: itemId })
       .getMany()
   }
 
   async findOneById(itemId: string, id: string): Promise<Revision | undefined> {
     return this.createQueryBuilder('revision')
       .where('revision.uuid = :uuid', { uuid: id })
-      .innerJoin(
-        'item_revisions',
-        'ir',
-        'ir.item_uuid = revision:item_uuid'
+      .innerJoinAndSelect(
+        'revision.items',
+        'item',
+        'item.uuid = :item_uuid',
+        { item_uuid: itemId }
       )
-      .andWhere('ir.item_uuid = :item_uuid', { item_uuid: itemId })
       .getOne()
   }
 }
