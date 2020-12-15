a070a9ce847daeb5546c26f1ae640888eb28b969
Karol Sójko <karolsojko@gmail.com>
2020-12-15 20:44:02 +0100

    fix: refactor auth response factories

--- src/Domain/Auth/CurrentAuthResponse.ts
+++
@@ -1,8 +0,0 @@
-import { SessionPayload } from '../Session/SessionPayload'
-import { KeyParams } from '../User/KeyParams'
-
-export type CurrentAuthResponse = {
-  session: SessionPayload
-  key_params: KeyParams
-  user: Record<string, unknown>
-}
