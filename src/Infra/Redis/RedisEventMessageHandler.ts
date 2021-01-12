49ff59abc9a9809906a9a4e08348918442dc74a3
Karol Sójko <karolsojko@gmail.com>
2021-01-12 11:13:18 +0100

    feat: add SQS and Redis event subscribers

---
+++ src/Infra/Redis/RedisEventMessageHandler.ts
@@ -0,0 +1,40 @@
+import * as zlib from 'zlib'
+
+import { inject, injectable } from 'inversify'
+import { EventMessageHandlerInterface } from '../../Domain/Event/EventMessageHandlerInterface'
+import { DomainEventHandlerInterface } from '../../Domain/Event/DomainEventHandlerInterface'
+import TYPES from '../../Bootstrap/Types'
+import { Logger } from 'winston'
+import { DomainEventInterface } from '../../Domain/Event/DomainEventInterface'
+
+@injectable()
+export class RedisEventMessageHandler implements EventMessageHandlerInterface {
+  constructor(
+    private handlers: Map<string, DomainEventHandlerInterface>,
+    @inject(TYPES.Logger) private logger: Logger
+  ) {
+  }
+
+  async handleMessage (message: string): Promise<void> {
+    try {
+      const domainEventJson = zlib.unzipSync(Buffer.from(message, 'base64')).toString()
+
+      const domainEvent: DomainEventInterface = JSON.parse(domainEventJson)
+
+      const handler = this.handlers.get(domainEvent.type)
+      if (!handler) {
+        this.logger.warn(`Event handler for event type ${domainEvent.type} does not exist`)
+
+        return
+      }
+
+      await handler.handle(domainEvent)
+    } catch (error) {
+      await this.handleError(error)
+    }
+  }
+
+  async handleError (error: Error): Promise<void> {
+    this.logger.error('Error occured while handling Redis message: %O', error)
+  }
+}
