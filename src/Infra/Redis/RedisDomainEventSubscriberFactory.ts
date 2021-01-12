f949a9abccf7b24de6652791990674ff711699f5
Karol Sójko <karolsojko@gmail.com>
2021-01-12 15:09:22 +0100

    feat: add syncing server js worker for async jobs

--- src/Infra/Redis/RedisDomainEventSubscriberFactory.ts
+++ src/Infra/Redis/RedisDomainEventSubscriberFactory.ts
@@ -1,12 +1,13 @@
 import * as IORedis from 'ioredis'
 
-import { inject } from 'inversify'
+import { inject, injectable } from 'inversify'
 import { DomainEventSubscriberFactoryInterface } from '../../Domain/Event/DomainEventSubscriberFactoryInterface'
 import { DomainEventSubscriberInterface } from '../../Domain/Event/DomainEventSubscriberInterface'
 import { RedisDomainEventSubscriber } from './RedisDomainEventSubscriber'
 import TYPES from '../../Bootstrap/Types'
 import { EventMessageHandlerInterface } from '../../Domain/Event/EventMessageHandlerInterface'
 
+@injectable()
 export class RedisDomainEventSubscriberFactory implements DomainEventSubscriberFactoryInterface {
   private readonly EVENT_CHANNEL = 'events'
 
