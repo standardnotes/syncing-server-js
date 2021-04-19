2109ba1d005d0d5520074874ff68399978bf88fd
Karol Sójko <karolsojko@gmail.com>
2021-04-19 15:09:39 +0200

    fix: add missing utc extensions to backup job

--- bin/backup.ts
+++ bin/backup.ts
@@ -3,6 +3,8 @@ import 'reflect-metadata'
 import './tracer'
 
 import { Logger } from 'winston'
+import * as dayjs from 'dayjs'
+import * as utc from 'dayjs/plugin/utc'
 
 import { ContainerConfigLoader } from '../src/Bootstrap/Container'
 import TYPES from '../src/Bootstrap/Types'
@@ -100,6 +102,8 @@ const runDailyBackup = async (
 
 const container = new ContainerConfigLoader
 void container.load().then(container => {
+  dayjs.extend(utc)
+
   const env: Env = new Env()
   env.load()
 
@@ -130,5 +134,4 @@ void container.load().then(container => {
 
       process.exit(1)
     })
-
 })
