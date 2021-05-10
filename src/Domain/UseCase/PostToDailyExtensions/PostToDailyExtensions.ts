42c59626c5baabaf23a02cf69da2cdf88ce8e6b5
Karol Sójko <karolsojko@gmail.com>
2021-05-10 17:08:01 +0200

    fix: add debug logs to posting to daily extensions

--- src/Domain/UseCase/PostToDailyExtensions/PostToDailyExtensions.ts
+++ src/Domain/UseCase/PostToDailyExtensions/PostToDailyExtensions.ts
@@ -1,5 +1,6 @@
 import { DomainEventInterface, DomainEventPublisherInterface } from '@standardnotes/domain-events'
 import { inject, injectable } from 'inversify'
+import { Logger } from 'winston'
 import TYPES from '../../../Bootstrap/Types'
 import { DomainEventFactoryInterface } from '../../Event/DomainEventFactoryInterface'
 import { Frequency } from '../../ExtensionSetting/Frequency'
@@ -17,10 +18,13 @@ export class PostToDailyExtensions implements UseCaseInterface {
     @inject(TYPES.ContentDecoder) private contentDecoder: ContentDecoderInterface,
     @inject(TYPES.DomainEventPublisher) private domainEventPublisher: DomainEventPublisherInterface,
     @inject(TYPES.DomainEventFactory) private domainEventFactory: DomainEventFactoryInterface,
+    @inject(TYPES.Logger) private logger: Logger
   ) {
   }
 
   async execute(dto: PostToDailyExtensionsDTO): Promise<PostToDailyExtensionsResponse> {
+    this.logger.debug('Posting to daily extensions...')
+
     const dailyExtensions = this.filterOutDailyExtensions(dto.items)
 
     for (const dailyExtension of dailyExtensions) {
@@ -28,6 +32,8 @@ export class PostToDailyExtensions implements UseCaseInterface {
 
       let event: DomainEventInterface
       if ('subtype' in decodedContent && decodedContent.subtype === ContentSubtype.BackupEmailArchive) {
+        this.logger.debug(`Publishing EMAIL_ARCHIVE_EXTENSION_SYNCED event for extension: ${dailyExtension.uuid}`)
+
         event = this.domainEventFactory.createEmailArchiveExtensionSyncedEvent(
           dto.userUuid,
           dailyExtension.uuid,
@@ -36,6 +42,8 @@ export class PostToDailyExtensions implements UseCaseInterface {
         if (!('url' in decodedContent)) {
           continue
         }
+
+        this.logger.debug(`Publishing ITEMS_SYNCED event for extension: ${dailyExtension.uuid}`)
 
         event = this.domainEventFactory.createItemsSyncedEvent({
           userUuid: dto.userUuid,
