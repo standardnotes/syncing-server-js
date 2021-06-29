3482e4d83fd9afad8461ab2c9d58838a23bd08e2
Karol Sójko <karolsojko@gmail.com>
2021-06-29 17:22:11 +0200

    feat: add saving mfa secret as user setting (#78)

--- src/Domain/Auth/AuthHttpServiceInterface.ts
+++ src/Domain/Auth/AuthHttpServiceInterface.ts
@@ -2,4 +2,6 @@ import { KeyParams } from '@standardnotes/auth'
 
 export interface AuthHttpServiceInterface {
   getUserKeyParams(dto: { email?: string, uuid?: string, authenticated: boolean }): Promise<KeyParams>
+  saveUserMFA(dto: { userUuid: string, mfaSecret: string }): Promise<string>
+  getUserMFA(userUuid: string): Promise<string>
 }
